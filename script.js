a4b9754bcf4c79e0ecf12dcea29a785f48f298f7
h7g5q9s2z2-droid <h7g5q9s2z2@privaterelay.appleid.com>
2026-01-13 16:02:39 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
 function pay() {
-  window.location.href =YOUR_CHECKOUT_LINK" https://buy.stripe.com/6oU6oI7642Nebil3ucgw000
-} ;
-}
+  window.location.href =https://buy.stripe.com/6oU6oI7642Nebil3ucgw000
+} 
